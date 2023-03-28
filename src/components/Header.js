ca5dcbabf4574f92862c76a0800251eaeedf437f
Dapo-Obembe <obembedapo@gmail.com>
2023-03-28 06:35:36 -0700

    Started with the Header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Nav from "../components/Nav";
 import logo from '../assets/img/Logo.svg';
+import '../assets/css/header.css';
 
 function Header() {
   return (
     <>
       <header>
-        <img src={logo} />
-        <Nav />
+        <div className="header__container">
+          <img src={logo} />
+          <Nav />
+        </div>
+        
       </header>
     </>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,18 @@
 import React from "react";
+import '../assets/css/header.css';
+
 
 function Nav() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Menu</li>
-          <li>Reservations</li>
-          <li>Order Online</li>
-          <li>Login</li>
+      <nav className="header__nav">
+        <ul className="header__nav--lists">
+          <li className="header__nav--item">Home</li>
+          <li className="header__nav--item">About</li>
+          <li className="header__nav--item">Menu</li>
+          <li className="header__nav--item">Reservations</li>
+          <li className="header__nav--item">Order Online</li>
+          <li className="header__nav--item loginBtn">Login</li>
         </ul>
       </nav>
     </>
